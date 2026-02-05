8fc459590bcaecde20ecbdce63a2e1fd6bdc0a17
Victor_Cuevas <kent2super9@gmail.com>
2026-02-04 22:00:35 -0600

    first commit

---
+++ users/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+import pytest
+from django.urls import reverse
+
+# Create your tests here.
+@pytest.fixture
+def test_user(db, django_user_model):
+    django_user_model.objects.create_user(
+        username="test_username",
+        password="test_password"
+    )
+    return "test_username", "test_password"
+def test_login_user(client, test_user):
+    test_username, test_password = test_user
+
+    login_result = client.login(
+        username=test_username,
+        password=test_password
+    )
+
+    assert login_result is True
